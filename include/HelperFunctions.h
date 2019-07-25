367b9a19d13dbf574352dbef7a0fdeca7b3e1098
Michael Brunner <michael.brunner@tum.de>
2019-07-25 22:48:30 +0200

    Added more helper functions to print tensors

--- include/HelperFunctions.h
+++ include/HelperFunctions.h
@@ -5,5 +5,17 @@
 
 namespace HelperFunctions {
     std::string toString(const Eigen::MatrixXf& mat);
+
+    /**
+     * Print the custom tensor format as split matrices.
+     * @param input The tensor to print.
+     */
+    std::string print_tensor(const Eigen::MatrixXf &input, int img_height, int img_width, int num_channels);
+
+    /**
+     * Print the first matrix/sample of the custom tensor format.
+     * @param input The tensor to print.
+     */
+    std::string print_tensor_first(const Eigen::MatrixXf &input, int img_height, int img_width, int num_channels);
 }
 
--- src/HelperFunctions.cpp
+++ src/HelperFunctions.cpp
@@ -1,7 +1,43 @@
 #include "HelperFunctions.h"
+#include <iostream>
+
 
 std::string HelperFunctions::toString(const Eigen::MatrixXf &mat) {
         std::stringstream ss;
         ss << mat;
         return ss.str();
+}
+
+std::string HelperFunctions::print_tensor(const Eigen::MatrixXf &input, int img_height, int img_width, int num_channels) {
+    std::stringstream ss;
+    ss << "Matrix of size: " << input.size() << "; Number of samples: " <<input.cols() << "; Images of height: " << img_height << ", width: " << img_width << ", channels: " << num_channels << std::endl;
+    for (int i =0; i < input.cols(); i++) {
+        ss << "\nSample: " << i <<std::endl;
+        for (int j=0; j< num_channels; j++) {
+            ss << "Channel: " << j << std::endl;
+            for (int k=0; k < img_height; k++) {
+                for (int l=0; l<img_width; l++) {
+                    ss << input(k+l*img_height+img_height*img_width*j,i) << " ";
+                }
+                ss << std::endl;
+            }
+        }
+    }
+    return ss.str();
+}
+
+std::string HelperFunctions::print_tensor_first(const Eigen::MatrixXf &input, int img_height, int img_width, int num_channels) {
+    std::stringstream ss;
+    ss << "Matrix of size: " << input.size() << "; Number of samples: " <<input.cols() << "; Images of height: " << img_height << ", width: " << img_width << ", channels: " << num_channels << std::endl;
+
+    for (int j=0; j< num_channels; j++) {
+        ss << "Channel: " << j << std::endl;
+        for (int k=0; k < img_height; k++) {
+            for (int l=0; l<img_width; l++) {
+                ss << input(k+l*img_height+img_height*img_width*j,0) << " ";
+            }
+            ss << std::endl;
+        }
+    }
+    return ss.str();
 }
