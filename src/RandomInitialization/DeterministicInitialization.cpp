279dd2e7da9f00e7be363b0f4574dff358a165c2
Michael Brunner <michael.brunner@tum.de>
2019-07-25 22:49:44 +0200

    Added possibility to initialize weights with ones

--- src/RandomInitialization/DeterministicInitialization.cpp
+++ src/RandomInitialization/DeterministicInitialization.cpp
@@ -4,6 +4,7 @@ void DeterministicInitialization::initialize(
     Eigen::Ref<Eigen::MatrixXf> input) const {
   srand(0);
   input.setRandom();
+  //input.setOnes();
   // input=Eigen::MatrixXf::Ones(input.rows(),input.cols());
   // input(1,0)=0;
 }
