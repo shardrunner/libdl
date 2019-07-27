d54e8ae7543a408d906f92f3afe3a317f7b366b1
Michael Brunner <michael.brunner@tum.de>
2019-07-27 21:33:58 +0200

    Logger switched to async logging

--- include/ManageLoggers.h
+++ include/ManageLoggers.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "spdlog/sinks/stdout_sinks.h" // or "../stdout_sinks.h" if no colors needed
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/spdlog.h"
+
 
 class ManageLoggers {
 public:
--- src/ManageLoggers.cpp
+++ src/ManageLoggers.cpp
@@ -1,26 +1,35 @@
 #include "ManageLoggers.h"
 
+#include "spdlog/async.h"
+#include "spdlog/sinks/stdout_sinks.h" // or "../stdout_sinks.h" if no colors needed
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
+
 #include <iostream>
 #include <memory>
 
 //from https://github.com/gabime/spdlog/wiki/1.-QuickStart
 void ManageLoggers::initLoggers() const {
     try {
+        spdlog::init_thread_pool(8192, 1);
         std::vector<spdlog::sink_ptr> sinks;
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_sink_st>();
+        auto console_sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
         console_sink->set_level(spdlog::level::warn);
         console_sink->set_pattern("[%H:%M:%S.%e] [%n] [%l] %v");
         //console_sink->set_pattern("[%^%l%$] %v");
 
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_st>("MlibLogs.txt", true);
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("MlibLogs.txt", true);
         file_sink->set_level(spdlog::level::trace);
         file_sink->set_pattern("[%H:%M:%S.%e] [%n] [%l] %v");
 
         sinks.push_back(std::move(console_sink));
         sinks.push_back(std::move(file_sink));
 
-        auto nn_logger=std::make_shared<spdlog::logger>("nn", begin(sinks), end(sinks));
-        auto convlayer_logger=std::make_shared<spdlog::logger>("convlayer", begin(sinks), end(sinks));
+        auto nn_logger = std::make_shared<spdlog::async_logger>("nn", begin(sinks), end(sinks),spdlog::thread_pool(),spdlog::async_overflow_policy::block);
+        auto convlayer_logger=std::make_shared<spdlog::async_logger>("convlayer", begin(sinks), end(sinks),spdlog::thread_pool(),spdlog::async_overflow_policy::block);
+
+        //auto nn_logger=std::make_shared<spdlog::logger>("nn", begin(sinks), end(sinks));
+        //auto convlayer_logger=std::make_shared<spdlog::logger>("convlayer", begin(sinks), end(sinks));
         //auto net_logger = std::make_shared<spdlog::logger>("net", file_sink);
         //spdlog::logger logger("multi_sink", {console_sink, file_sink});
         convlayer_logger->set_level(spdlog::level::debug);
