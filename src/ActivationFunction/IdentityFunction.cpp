043ad71d593f3700f9e646f4297d510b391326ba
Michael Brunner <michael.brunner@tum.de>
2019-08-14 20:24:57 +0200

    Activation: suppress identity function unused variable warnings

--- src/ActivationFunction/IdentityFunction.cpp
+++ src/ActivationFunction/IdentityFunction.cpp
@@ -1,9 +1,9 @@
 #include "ActivationFunction/IdentityFunction.h"
 
-void IdentityFunction::forward_propagation(Eigen::MatrixXf &input) const {}
+void IdentityFunction::forward_propagation([[maybe_unused]] Eigen::MatrixXf &input) const {}
 
 Eigen::MatrixXf
-IdentityFunction::apply_derivative(const Eigen::MatrixXf &m_a,
+IdentityFunction::apply_derivative([[maybe_unused]] const Eigen::MatrixXf &m_a,
                                    const Eigen::MatrixXf &dC_da) const {
   return dC_da;
 }
